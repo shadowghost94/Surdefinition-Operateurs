678723727b0636cdd52328159f8c81bec8731b5e
shadowghost94 <salifououzerou25@gmail.com>
2023-08-13 23:33:02 +0100

    Surdéfinition de l'opérateur []

---
+++ exo89.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class vecteur3d
+{
+    float v[3];
+public:
+    vecteur3d(float c1=0.0, float c2=0.0, float c3=0.0)
+    {
+        v[0]= c1;
+        v[1]= c2;
+        v[2]= c3;
+    }
+
+    operator []()
+}
